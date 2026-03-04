2b5cc0719723a36b1a08f4d855e8563909ef8766
bender <kudin.dima@gmail.com>
2026-03-04 08:37:59 +0000

    Update src/components/navbar/NavbarLayoutFloatingOverlay/NavbarLayoutFloatingOverlay.tsx

--- src/components/navbar/NavbarLayoutFloatingOverlay/NavbarLayoutFloatingOverlay.tsx
+++ src/components/navbar/NavbarLayoutFloatingOverlay/NavbarLayoutFloatingOverlay.tsx
@@ -28,7 +28,7 @@ const NavbarLayoutFloatingOverlay = ({
   // logoSrc,
   // logoAlt = "",
   className = "",
-  brandName = "Webild",
+  brandName = "Wdebild",
   button,
   buttonClassName = "",
   buttonTextClassName = "",
